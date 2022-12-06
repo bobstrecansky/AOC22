ede6e59e846384cf56afc24fd5330332cabb54d3
Bob Strecansky <bob.strecansky@gmail.com>
2022-12-06 09:57:33 -0500

    Day 6

---
+++ 6/part_one/day_6_part_one.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	// Read input file
+	input, err := os.Open("input.txt")
+	defer func(input *os.File) {
+		err := input.Close()
+		if err != nil {
+			log.Fatalf("Failed reading File %s", err)
+		}
+	}(input)
+	if err != nil {
+		log.Fatalf("Failed reading File %s", err)
+	}
+	fileScanner := bufio.NewScanner(input)
+	fileScanner.Scan()
+
+	var buffer int = 4
+
+	for i := range fileScanner.Text() {
+		chars := make(map[byte]bool)
+		for j := 0; j < buffer; j++ {
+			chars[fileScanner.Text()[i+j]] = true
+		}
+
+		if len(chars) == buffer {
+			fmt.Println(i + buffer)
+			break
+		}
+	}
+}
